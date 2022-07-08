220dce26246d7d62e0681a5072ed2fd34152fd14
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2022-07-08 00:54:17 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@
 
 
 # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-PASS = "65521m5352342486Mfc"
+PASS = "e"
 DATABASE_URI = 'postgresql+psycopg2://postgres:'+ PASS+ '@localhost:5432/tablas_alkemy'
